be444fe21a30cb195a5cb2c1c5f9f62c2d1cd888
Alessandro Grassi <a.grassi@ComSDBM01.local>
2025-11-11 14:39:35 +0100

    fix: rimosso extension duplicate da AIBehaviors.swift

--- Orbitica Core/AIBehaviors.swift
+++ Orbitica Core/AIBehaviors.swift
@@ -386,35 +386,3 @@ class OrbitPlanetBehavior: AIBehavior {
         )
     }
 }
-
-// MARK: - Utility Extensions
-
-extension CGVector {
-    var length: CGFloat {
-        return sqrt(dx * dx + dy * dy)
-    }
-    
-    func normalized() -> CGVector {
-        let len = length
-        guard len > 0 else { return .zero }
-        return CGVector(dx: dx / len, dy: dy / len)
-    }
-    
-    func dot(_ other: CGVector) -> CGFloat {
-        return dx * other.dx + dy * other.dy
-    }
-}
-
-extension CGPoint {
-    func distance(to other: CGPoint) -> CGFloat {
-        let dx = other.x - x
-        let dy = other.y - y
-        return sqrt(dx * dx + dy * dy)
-    }
-    
-    func angle(to other: CGPoint) -> CGFloat {
-        let dx = other.x - x
-        let dy = other.y - y
-        return atan2(dy, dx)
-    }
-}
